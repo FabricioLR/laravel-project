a1bc8058d0d529e2627f0f52987b04c9315b5a5a
FabricioLR <fabriciolongodearaujo@gmail.com>
2026-04-29 14:22:03 -0300

    fix: adicionando passagem de comandos dinamicos para entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-
 if [ ! -d "vendor" ]; then
     composer install --no-interaction --optimize-autoloader
 fi
-
 if [ ! -d "node_modules" ]; then
     npm install
 fi
-
-php artisan key:generate --show
-
-php artisan migrate
-
 npm run dev -- --host &
-
-exec php artisan serve --host=0.0.0.0 --port=8000
+exec "$@"
